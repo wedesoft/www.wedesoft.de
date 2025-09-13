aebf95ac4459af0ad9544e00af25c506c851af0b
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-09-13 01:41:10 +0100

    Remove lftp.sh

--- cert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo certbot certonly --manual --preferred-challenges http
-# cert.pem and privkey.pem at /etc/letsencrypt/live/www.wedesoft.de
--- lftp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-lftp -u ftp12768462-web118 wp12768462.server-he.de
